8fa971b954af1185184e6fdb64bcc13dddbe77bc
Anas Sabri <anassabri@gmail.com>
2025-02-17 13:04:04 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google"
 import type { ReactNode } from "react"
 import './globals.css'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
